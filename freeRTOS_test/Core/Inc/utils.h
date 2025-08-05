5872380491452905c168cd970e9dcad959882063
agheles <agheles4mekdache@gmail.com>
2025-08-05 20:18:11 +0100

    little game with freeRTOS

---
+++ freeRTOS_test/Core/Inc/utils.h
@@ -0,0 +1,13 @@
+/*
+ * utils.h
+ *
+ *  Created on: Jul 22, 2025
+ *      Author: agheles
+ */
+
+#ifndef INC_UTILS_H_
+#define INC_UTILS_H_
+
+
+
+#endif /* INC_UTILS_H_ */
---
+++ freeRTOS_test/Core/Src/utils.c
@@ -0,0 +1,7 @@
+/*
+ * utils.c
+ *
+ *  Created on: Jul 22, 2025
+ *      Author: agheles
+ */
+
